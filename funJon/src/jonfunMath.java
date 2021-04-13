f6a3ddd03e4c179b0179bb7bc0ef0f104ed8ca5e
Alex-shadow1005 <kaareudlejningr@gmail.com>
2021-04-13 16:41:00 +0200

    funJON

---
+++ funJon/src/jonfunMath.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class jonfunMath {
+    public static void main(String[] args) {
+        int num;
+        System.out.println("insæt nummer ");
+
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
+
+        if (num %2 == 0){
+            System.out.println("lige tal " + num);
+        } else {
+            System.out.println("ulige tal " + num);
+        }
+
+
+
+    }
+        }
+
